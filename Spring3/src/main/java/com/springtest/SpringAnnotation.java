8b11e4741b7728f8759f76b01f49fe2ba3e1b3b3
zero <zero1209556535@qq.com>
2016-10-18 14:43:26 +0800

    还原

--- Spring3/src/main/java/com/springtest/SpringAnnotation.java
+++ Spring3/src/main/java/com/springtest/SpringAnnotation.java
@@ -12,7 +12,6 @@ public class SpringAnnotation {
         System.out.println("zhuzl edit");
         System.out.println("this is spring test2");
         System.out.println("zzl edit spring test2");
-        System.out.println("this is spring test3");
     }
 
 }
